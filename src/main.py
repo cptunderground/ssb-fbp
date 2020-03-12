a5048894c601fba3d6ed22da2e73f7178f9957e9
cptunderground <jannik.jaberg@unibas.ch>
2020-03-12 14:44:44 +0100

    initialises ssb-log as .json file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,42 @@
+import os
+import json
 import node
 import networkx as nx
 
+
+def initLog(verbose: bool = False):
+    print('start retrieving your log')
+
+    #starting sbot server
+    '''
+    cmd = 'sbot start'
+    os.popen(cmd)
+    '''
+
+
+    cmd = 'sbot whoami'
+    stream = os.popen(cmd)
+    print('executing command:', cmd)
+    output = stream.read()
+    id = json.loads(output)
+    print('extracted ID:', id['id'])
+
+    cmd = 'sbot createUserStream --id ' + id['id']
+    if (verbose):
+        print('executing command:', cmd)
+        stream = os.popen(cmd)
+        output = stream.read()
+        print(output)
+    os.popen(cmd + ' > log.json')
+    print('executed command:', cmd)
+    print('log saved as log.json')
+
+
 if __name__ == '__main__':
+
+    initLog(verbose=False)
+
+    '''
     basel = node.ISP('basel', 3)
     zurich = node.ISP('zürich', 7)
     jb = node.butt('jb', 5)
@@ -24,3 +59,4 @@ if __name__ == '__main__':
     g = nx.Graph()
     g.add_edge(jb, basel)
     print(g)
+    '''
